c643974f543f1103a1a2c1ec3e7e116df2b4c7bb
mocmjmm6 <mocmjmm6@E-LOSXF37P5.local>
2025-03-28 13:35:15 +0000

    Updates and checking that commit messages are actually coming through now!

--- build-all.sh
+++ build-all.sh
@@ -3,8 +3,8 @@
 rm -rf ./_build
 jupyter-book build ./
 echo Enter message for commit
-read message 
+read commitMessage 
 git add -A
-git commit -m '$message'
+git commit -m "$commitMessage"
 git push
 ghp-import -n -p -f _build/html
